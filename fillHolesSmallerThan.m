dae13ed3c409dfb1d337ce8af2c845e06b3de820
drunkeNNNN <99848935+drunkeNNNN@users.noreply.github.com>
2022-10-09 17:39:33 +0200

    Delete fillHolesSmallerThan.m

--- fillHolesSmallerThan.m
+++
@@ -1,5 +0,0 @@
-function bwSmallHolesFilled= fillHolesSmallerThan(bwImage, minimumHoleSize)
-    bwHoles=imfill(bwImage,'holes') & ~ bwImage;
-    bwTooSmallHoles=bwHoles & ~bwareaopen(bwHoles,minimumHoleSize);
-    bwSmallHolesFilled=bwImage|bwTooSmallHoles;
-end
